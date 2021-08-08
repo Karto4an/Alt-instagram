f19890c7ca1ef3b8d895b0df6ded3b035d05892a
Karto4an <1sasha.ignatenko1@gmail.com>
2021-08-08 17:32:41 +0300

    Add files via upload

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,4 +1,19 @@
 console.log('JS is working');
 var scrollbar = document.getElementById('videos-scroll');
-// scrollbar.scrollBy(100, 0)
-console.log(scrollbar);
+var shrt_vd_arrw_l = document.getElementById('sva_l');
+var shrt_vd_arrw_r = document.getElementById('sva_r');
+shrt_vd_arrw_l.addEventListener('click', event => {
+    console.log('left');
+    scrollbar.scrollBy({
+        left: -600,
+        behavior: "smooth"
+    });
+  });
+
+shrt_vd_arrw_r.addEventListener('click', event => {
+    console.log('right');
+    scrollbar.scrollBy({
+        left: 600,
+        behavior: "smooth"
+    });
+  });
