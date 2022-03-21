75ff04d47f127a0fae2f608dc01b18a7ddcc75cf
Karto4an <ikarto4an@gmail.com>
2022-03-21 17:33:22 +0200

    Dapp

--- server/routes/auth_routes.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const authController = require('../controller/auth_controller.js')
-
-router.get('/auth', authController.getSessionToken)
-router.post('/auth', authController.checkSessionToken)
-router.delete('/auth', authController.removeSessionToken)
-
-module.exports = router
--- server/routes/post_routes.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const postController = require('../controller/post_controller.js')
-
-router.post('/post', postController.createPost)
-router.get('/post', postController.getPostsByUser)
-router.get('/post/test', postController.testPosts)
-
-module.exports = router
