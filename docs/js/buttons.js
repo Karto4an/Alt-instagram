0d41db4a251e4b87bef2db20a88a84d179fc5fae
Karto4an <ikarto4an@gmail.com>
2022-03-24 16:42:24 +0200

    Popup blocker

--- docs/js/buttons.js
+++ docs/js/buttons.js
@@ -5,6 +5,7 @@ const login_dialog = document.getElementById("popup-login");
 
 login_button.addEventListener("click", event => {
     login_dialog.classList.add("show");
+    popup_blocker.classList.add("show");
 });
 
 
@@ -13,6 +14,7 @@ login_button.addEventListener("click", event => {
 const login_with_metamask_button = document.getElementById("metamask-login");
 const providers_list = document.getElementById("providers-list");
 const popup_alert = document.getElementById("popup-alert");
+const popup_blocker = document.getElementById("popup-blocker");
 
 login_with_metamask_button.addEventListener("click", event => {
     providers_list.classList.add("hide");
@@ -21,6 +23,15 @@ login_with_metamask_button.addEventListener("click", event => {
         login_dialog.classList.remove("show");
         providers_list.classList.remove("hide");
         popup_alert.classList.add("hide");
+        popup_blocker.classList.remove("show");
         console.log("DONE");
     });
+});
+
+
+// TRACK BACKGROUND CLICKS
+
+popup_blocker.addEventListener("click", event => {
+    popup_blocker.classList.remove("show");
+    login_dialog.classList.remove("show");
 });
